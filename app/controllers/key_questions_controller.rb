1144bbac8aa609c4f2de3665c0a35881c315536a
unknown <cparkin@.tufts-nemc.org>
2010-10-26 15:46:03 -0400

    made some changes to key_questions forms for edit functionality. now uses ajax for forms. problem lies when you delete a record you're currently editing.

--- app/controllers/key_questions_controller.rb
+++ app/controllers/key_questions_controller.rb
@@ -3,7 +3,7 @@ class KeyQuestionsController < ApplicationController
   # GET /key_questions.xml
   def index
     #@key_questions = KeyQuestion.all
-	@key_questions = KeyQuestion.where(["project_id = ?", session[:project_id]])
+	  @key_questions = KeyQuestion.where(["project_id = ?", session[:project_id]])
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @key_questions }
@@ -26,8 +26,12 @@ class KeyQuestionsController < ApplicationController
   # GET /key_questions/new.xml
   def new
     @key_question = KeyQuestion.new
-	
     respond_to do |format|
+    	format.js{
+    	  render :update do |page|
+    	  	page.replace_html 'key_question_form', :partial => 'key_questions/new_kq_form'
+    	  end
+  	  }
       format.html # new.html.erb
       format.xml  { render :xml => @key_question }
     end
@@ -36,6 +40,13 @@ class KeyQuestionsController < ApplicationController
   # GET /key_questions/1/edit
   def edit
     @key_question = KeyQuestion.find(params[:id])
+    respond_to do |format|
+      format.js {
+		  	render :update do |page|
+					page.replace_html 'key_question_form', :partial => 'key_questions/edit_kq_form'
+				end
+			}
+	  end
   end
 	
   # POST /key_questions
@@ -71,7 +82,14 @@ class KeyQuestionsController < ApplicationController
 
     respond_to do |format|
       if @key_question.update_attributes(params[:key_question])
-        format.html { redirect_to(project_key_question_path(session[:project_id],@key_question), :notice => 'Key question was successfully updated.') }
+        @key_questions = KeyQuestion.where(:project_id => session[:project_id]).all 
+      	format.js {
+		  		render :update do |page|
+						page.replace_html 'key_question_table', :partial => 'key_questions/table'
+						page.replace_html 'key_question_form', :partial => 'key_questions/edit_kq_form'
+					end
+				}
+      	format.html { redirect_to(project_key_question_path(session[:project_id],@key_question), :notice => 'Key question was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -89,7 +107,8 @@ class KeyQuestionsController < ApplicationController
       format.js {
 				@key_questions = KeyQuestion.where(:project_id => session[:project_id]).all
 		  	render :update do |page|
-					page.replace_html 'key_question_table', :partial => 'key_questions/table'
+					page.replace_html 'key_question_table', :partial => 'key_questions/table'	
+					KeyQuestionsController.new
 		  	end
 			}
       #format.html { redirect_to( project_key_question_path(session[:project_id]) )}
