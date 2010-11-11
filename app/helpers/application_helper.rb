0f157641e37244b6443c72c0eeed0456a5f82871
unknown <cparkin@.tufts-nemc.org>
2010-11-11 11:57:47 -0500

    Let there be breadcrumbs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,89 @@
 module ApplicationHelper
 	
-	def get_bread_crumbs(url)
+	def get_bread_crumbs(url,sep)
+	begin
+		home_uri = "/projects/"
+		elements = url.split('/')
+		project_id = ""
+		project_title = ""
+		study_id = ""
+		study_title = ""
+		no_match = false
+		is_end_of_trail = false
 		
-	
+		# the string to return 
+		retVal = "<a href='\#'>Home</a> "
 		
-		#home_uri = "/projects/"
-		#project_name = Project.find(session[:project_id], :select="title")
-		#project_uri = "/projects/" + session[:project_id]
-		return  url
+		# remove the http://www.srdr.com bit from the array
+		elements = elements[3..elements.length-1]
+		
+		i = 0
+		elements.each do |element|
+			if i == elements.length - 1
+				is_end_of_trail = true
+			end
+			retVal = retVal + '> '
+			case element
+			  when "projects"
+			  	project_uri	= "/projects/"
+				  retVal = retVal + create_crumb_link(project_uri, "Projects", is_end_of_trail)
+				when "studies"
+					study_uri = "/projects/#{project_id.to_s}/studies"
+				  retVal = retVal + create_crumb_link(study_uri,"Studies", is_end_of_trail)
+			  when /^\d+/
+			  	  
+			  	  previous = elements[i-1]
+						
+					  if previous == "projects"
+					  	project_id = element.to_i
+					  	project_title = Project.find(project_id,:select=>"title").title
+							project_uri = "/projects/#{project_id.to_s}"
+							retVal = retVal + create_crumb_link(project_uri, project_title, is_end_of_trail)
+												
+						elsif previous == "studies"
+							study_id = element.to_i
+							study_title = Publication.find(:first,:conditions=>["study_id=? AND is_primary=?",study_id.to_i,"t"],:select=>"title").title
+							study_uri = "/projects/#{project_id.to_s}/studies/#{study_id.to_s}"
+							retVal = retVal + create_crumb_link(study_uri, study_title, is_end_of_trail)	
+						else
+							
+						end
+					    
+				when /^\w/
+					uri = "/projects/#{project_id.to_s}/studies/#{study_id.to_s}/#{element.to_s}"
+				  text = element.to_s.capitalize  
+				  retVal = retVal + create_crumb_link(uri,text, is_end_of_trail)
+				  
+				else
+					# get rid of the trailing characters
+					# is doing two chops more efficient?
+					retVal.gsub!(/\>\s$/,"")
+					no_match = true
+			end
+			retVal = retVal + " "
+			i = i+1
+		end
+		return  retVal
+		
+	rescue
+  	return ""
+  end
 	end
 	
+	def create_crumb_link(uri,text,end_of_trail)
+		print "------------CREATING THE LINK--------------"
+		link = ""
+		if end_of_trail
+			link = "<span style='font-weight: bold;'>#{text.to_s}</span>"
+		else
+			link = '<a href='+uri.to_s+'>'+shrink_text(text.to_s)+'</a>'
+		end
+		return link
+	end
 	
-	def get_bread_crumb2(url)
-	  begin
-	    breadcrumb = ''
-	    sofar = '/'
-	    elements = url.split('/')
-	    for i in 1...elements.size
-	      sofar += elements[i] + '/'
-	      if i%2 == 0
-	        begin
-	          breadcrumb += "<a href='#{sofar}'>"  + eval("#{elements[i - 1].singularize.camelize}.find(#{elements[i]}).name").to_s + '</a>'
-	        rescue
-	          breadcrumb += elements[i]
-	        end
-	      else
-	        breadcrumb += "<a href='#{sofar}'>#{elements[i].pluralize}</a>"
-	      end
-	      breadcrumb += ' -> ' if i != elements.size - 1
-	    end
-	    breadcrumb
-	  rescue
-	    'Not available'
-	  end
+	def shrink_text(text)
+		words = 5
+		text = text.split[0..(words-1)].join(" ") + (text.split.size > words ? "..." : "")
+		return text
 	end
 end
