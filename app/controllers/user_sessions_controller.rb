cdd08e0338cc31470da33f7ff71d4ccdee4d3dc7
unknown <sarahjanekeefe@gmail.com>
2010-11-17 15:37:32 -0500

    login fixing

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,7 +12,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default ""
+      redirect_back_or_default "/userhome"
     else
       render :action => :new
     end
