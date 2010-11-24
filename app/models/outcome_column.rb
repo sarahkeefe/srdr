607d900cf37b806439e03b40d761ea1c5d6f65af
unknown <sarahjanekeefe@gmail.com>
2010-11-24 15:10:30 -0500

    merging

--- app/models/outcome_column.rb
+++ app/models/outcome_column.rb
@@ -2,4 +2,9 @@ class OutcomeColumn < ActiveRecord::Base
 	belongs_to :outcome
 	attr_accessible :name, :description
 	validates :name, :presence => true
+	
+	def self.get_custom_columns(outcome_id, timepoint_id, subgroup_id)
+		@columns = OutcomeColumn.where(:outcome_id => outcome_id, :timepoint_id => timepoint_id, :subgroup_id => subgroup_id).all
+		return @columns
+	end
 end
